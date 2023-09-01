7d0a71e62ab04ee140c3d213a8bc9d7bdedecb78
Robin Östlund <me@robinostlund.name>
2023-09-01 10:37:26 +0200

    updating readme

---
+++ tests/test_fetch_data.py
@@ -0,0 +1,75 @@
+"""Test to fetch data from mimer"""
+import pytest
+import logging
+from datetime import date
+
+from aiosvkmimer.client import Mimer
+
+# settings
+AVAILABLE_KW = 8
+LOGGING_LEVEL = logging.INFO
+PERIOD_FROM = date.today().strftime('%Y-%m-%d')
+PERIOD_TO = date.today().strftime('%Y-%m-%d')
+
+# configure logging
+logging.basicConfig(
+    format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    level=LOGGING_LEVEL
+)
+
+@pytest.mark.asyncio
+async def test_exchange_rates():
+    """Test to get exchange rates from mimer"""
+    mimer = Mimer(
+        kw_available=AVAILABLE_KW
+    )
+
+    await mimer.fetch(
+        period_from=PERIOD_FROM,
+        period_to=PERIOD_TO
+    )
+
+    assert len(mimer.process_exchange_rates()) is not 0
+
+@pytest.mark.asyncio
+async def test_fcr_n_prices():
+    """Test to get fcr n prices from mimer"""
+    mimer = Mimer(
+        kw_available=AVAILABLE_KW
+    )
+
+    await mimer.fetch(
+        period_from=PERIOD_FROM,
+        period_to=PERIOD_TO
+    )
+
+    assert len(mimer.get_fcr_n_prices()) is not 0
+
+@pytest.mark.asyncio
+async def test_fcr_d_up_prices():
+    """Test to get fcr up prices from mimer"""
+    mimer = Mimer(
+        kw_available=AVAILABLE_KW
+    )
+
+    await mimer.fetch(
+        period_from=PERIOD_FROM,
+        period_to=PERIOD_TO
+    )
+
+    assert len(mimer.get_fcr_d_up_prices()) is not 0
+
+@pytest.mark.asyncio
+async def test_fcr_d_down_prices():
+    """Test to get fcr down prices from mimer"""
+    mimer = Mimer(
+        kw_available=AVAILABLE_KW
+    )
+
+    await mimer.fetch(
+        period_from=PERIOD_FROM,
+        period_to=PERIOD_TO
+    )
+
+    assert len(mimer.get_fcr_d_down_prices()) is not 0
